3947b975121e255777fec9f4ce59b4fdf0668dd4
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-18 00:57:54 +0530

    Update Layout.js

--- admin-panel/components/Layout.js
+++ admin-panel/components/Layout.js
@@ -1,1 +1,19 @@
+import Header from './Header'
+import Footer from './Footer'
+import Sidebar from './Sidebar'
+
+const Layout = ({ children }) => {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Sidebar />
+            <div style={{ flex: 1 }}>
+                <Header />
+                <main>{children}</main>
+                <Footer />
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
 
